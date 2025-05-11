f5a64b73c04b5d62e151e7c1757a8b611a6613eb
Mahsa-jmorganLab <111803683+MahsaBank@users.noreply.github.com>
2025-05-11 11:15:16 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,3 +3,8 @@ import os
 
 
 def unzip_data(zip_path="data.zip", extract_to="data"):
+  if not os.path.exist(extract_to):
+    os.makedirs(extract_to)
+  with zipFile.zipFile(zip_path, "r") as zip_file:
+    zip_file.extractall(extract_to)
+  print(f"unzipped to {extract_to}")
